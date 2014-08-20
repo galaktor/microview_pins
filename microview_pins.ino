3f42c3fcd35c25030f6a53c3c21ce6c56b415239
Raphael Estrada <galaktor@gmx.de>
2014-08-20 12:54:40 +0100

    pad pin nr text nicer

--- microview_pins.ino
+++ microview_pins.ino
@@ -1,7 +1,3 @@
-/*  Copyright 2014, Raphael Estrada
-    Author email:   <galaktor@gmx.de>
-    Project home:   <https://github.com/galaktor/microview_pins>
-    Licensed under The GPL v3 License (see README and LICENSE files) */
 #include <MicroView.h>
 
 const int BOX_HEIGHT = 8;
@@ -13,11 +9,14 @@ const int ROW_BTM = 47-BOX_HEIGHT;
 const int COL_CTR = 32;
 
 const int PIN_CNT=12;
+const int ANALOG_OFFSET=14;
 
 /*     MV PIN                       09       10       11       12       13       14       07       06       05       04       03       02    */
 const int pins_ar[PIN_CNT] = {       0,       1,       2,       3,       5,       6,      A0,      A1,      A2,      A3,      A4,      A5 }; // arduino pin nr
 const int boxes_x[PIN_CNT] = {      56,      48,      40,      32,      24,      16,      48,      40,      32,      24,      16,       8 }; // x coordinate for box
 const int boxes_y[PIN_CNT] = { ROW_TOP, ROW_TOP, ROW_TOP, ROW_TOP, ROW_TOP, ROW_TOP, ROW_BTM, ROW_BTM, ROW_BTM, ROW_BTM, ROW_BTM, ROW_BTM }; // y coordinate for box
+
+const int TXT_PAD_L=2;
 
 void setup() {
     // set all pins to input
@@ -42,11 +41,11 @@ void loop() {
   for(int i=0; i<PIN_CNT; i++) {
      // access above arrays to read and draw here 
      if(boxes_y[i] == ROW_TOP) {
-       uView.setCursor(boxes_x[i], boxes_y[i]+BOX_HEIGHT+2);       
+       uView.setCursor(boxes_x[i]+TXT_PAD_L, boxes_y[i]+BOX_HEIGHT+2);       
        uView.print(pins_ar[i]);
      } else {
-       uView.setCursor(boxes_x[i], boxes_y[i]-BOX_HEIGHT-2);
-       uView.print(pins_ar[i]-7);
+       uView.setCursor(boxes_x[i]+TXT_PAD_L, boxes_y[i]-BOX_HEIGHT-2);
+       uView.print(pins_ar[i]-ANALOG_OFFSET);
      }
      if(digitalRead(pins_ar[i])) {
         // >0, therefore fill
